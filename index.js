518dcc65286aed4b58dfdf33b50f623e38cce0a3
malleeshkanna <88141142+malleeshkanna@users.noreply.github.com>
2022-12-11 23:27:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app=express();
 const request=require('request');
 const url='https://intriguebillingsoft-default-rtdb.firebaseio.com/';
 const header= {'Content-Type': 'application/x-www-form-urlencoded'};
-var timeout=2000;
 
 
 app.use(express.json());
@@ -19,13 +18,10 @@ app.use(function (req, res, next) {
 
 app.get('/biller_name',function(req,res){
     var names;
-    request({url:url+'billername.json'},(err,res)=>{
-        names=JSON.parse(res.body);
-    })
-    setTimeout(() => {
+    request({url:url+'billername.json'},(err,response)=>{
+        names=JSON.parse(response.body);
         res.send(names);  
-    }, timeout);
-    
+        })
     })
 
 app.post('/save_bill',function(req,res){
@@ -37,63 +33,53 @@ app.post('/save_bill',function(req,res){
 
 app.get('/print_bill',function(req,res){
         var printdata=[];
-        request({url:url+'bills.json'},(err,res)=>{
-            const data=JSON.parse(res.body);
+        request({url:url+'bills.json'},(err,response)=>{
+            const data=JSON.parse(response.body);
             printdata=[...new Map(Object.entries(data)).values()];
-        })
-        setTimeout(() => {
             res.send({status:true,message:"Bill Fetched Successfully",data:printdata[printdata.length-1]});
-        }, timeout);
+        }) 
 })
 
 
 app.get('/bill_list',function(req,res){
     var allbill=[];
-    request({url:url+'bills.json'},(err,res)=>{
-        const data=JSON.parse(res.body);
+    request({url:url+'bills.json'},(err,response)=>{
+        const data=JSON.parse(response.body);
         allbill=[...new Map(Object.entries(data)).values()];
-    })
-    setTimeout(() => {
         res.send({status:true,message:"Bill Fetched Successfully",data:allbill});
-    }, timeout);
+    })
 })
 
 app.post('/get_bill',function(req,res){
     var selectedbill=[];
     var lastdata=[];
-    request({url:url+'bills.json'},(err,res)=>{
-        const data=JSON.parse(res.body);
+    request({url:url+'bills.json'},(err,response)=>{
+        const data=JSON.parse(response.body);
         selectedbill=[...new Map(Object.entries(data)).values()];
-        lastdata.push(selectedbill.filter((el)=>el.invoice_number==req.body.getterid))
-    })
-    setTimeout(() => {
+        lastdata.push(selectedbill.filter((el)=>el.invoice_number==req.body.getterid));
         res.send({status:true,message:"Bill Fetched Successfully",data:lastdata[0][0]});
-    }, timeout);
+    })
 })
 
 
 app.post('/password',function(req,res){
     var passfilter=[];
-    request({url:url+'password.json'},(err,res)=>{
-        const data=JSON.parse(res.body);
+    request({url:url+'password.json'},(err,response)=>{
+        const data=JSON.parse(response.body);
         passfilter=[...new Map(Object.entries(data)).values()];
-    })
-    setTimeout(() => {
         if(passfilter[passfilter.length-1].password==req.body.password){
             res.send({status:true,message:"password is Correct"});
         }else{
             res.send({status:false,message:"password is Incorrect"});
         }
-    }, timeout);
+    })
 })
 
 app.post('/resetpassword',function(req,res){
     var resetpassfilter=[];
-    request({url:url+'password.json'},(err,res)=>{
-        const data=JSON.parse(res.body);
+    request({url:url+'password.json'},(err,response)=>{
+        const data=JSON.parse(response.body);
         resetpassfilter=[...new Map(Object.entries(data)).values()];
-    })
-    setTimeout(() => {
         if(resetpassfilter[resetpassfilter.length-1].password==req.body.oldpassword){
             const obj={password:req.body.newpassword};
             request.post({url:url+'password.json',form:JSON.stringify(obj),headers:header},function(error,httpResponse,body){
@@ -102,7 +88,7 @@ app.post('/resetpassword',function(req,res){
         }else{
             res.send({status:false,message:"Old Password is Incorrect"});
         }
-    }, timeout);
+    })
 })
 
 app.listen(3000,()=>{
